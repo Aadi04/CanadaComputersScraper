97cf7b06c15b18e0ca4676824aeeeee011eeb790
Aadi Sanghani <42876004+Aadi04@users.noreply.github.com>
2021-06-23 17:23:54 -0400

    test

--- scraper.py
+++ scraper.py
@@ -20,25 +20,27 @@ def get_content_from_canada_computers():
    attempts = 0
    
    while (True):
-        result = soup.find_all('i', attrs = {'class' : 'fas fa-ban red text-danger'})
+        # result = soup.find_all('i', attrs = {'class' : 'fas fa-ban red text-danger'})
+        result = soup.find_all('i')
         
         print (result)
         
-        if len(result) == 2: 
-            # Not in Stock
-            print('Time = ' + str(datetime.now()) + "-Attempt = " + str(attempts))
-            attempts += 1
-            time.sleep(4)
+        time.sleep(10)
+        # if len(result) == 2: 
+        #     # Not in Stock
+        #     print('Time = ' + str(datetime.now()) + "-Attempt = " + str(attempts))
+        #     attempts += 1
+        #     time.sleep(4)
         
-        elif len(result) > 2:
-            #Incase something weird happens 
-            publish('Something weird has happend in the Canada Computers Script; check the website out just incase something is broken or it is in stock')
-            break
-        else:
-            #Instock!! 
-            # publish('Lets gooo its in stock in Canada Computers!!! Goooooo!')
-            print("In stock")
-            time.sleep(10)
+        # elif len(result) > 2:
+        #     #Incase something weird happens 
+        #     publish('Something weird has happend in the Canada Computers Script; check the website out just incase something is broken or it is in stock')
+        #     break
+        # else:
+        #     #Instock!! 
+        #     # publish('Lets gooo its in stock in Canada Computers!!! Goooooo!')
+        #     print("In stock")
+        #     time.sleep(10)
     
 def publish(message):
     arn = 'arn:aws:sns:ca-central-1:616203313326:InStockTopic'
